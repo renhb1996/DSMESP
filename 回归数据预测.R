0cae757b5a717cff0e87eecda399cae02d126187
renhb1996 <52602088+renhb1996@users.noreply.github.com>
2021-02-20 22:23:22 +0800

    Add files via upload

--- 回归数据预测.R
+++ 回归数据预测.R
@@ -1,23 +1,10 @@
-load("C:/r_project/甜味绘图/RF_Model.RData")#调用模型
-#desc<-colnames(Dataframe_x)
-#write.table(desc,file="desc.txt",sep = ",")
-desc_value<-read.table("C:/r_project/甜味绘图/desc-hg.txt",sep = ",")[,1]#调用描述符
-#unknowsweet$id<-c(1:24735)
-data_select<-sweetvaluepred[,desc_value]
-data_select_scale <- scale(data_select)
-data_value_pred<- data.frame(SMILES = sweetvaluepred$SMILES,data_select_scale)
-sweetvaluepred.prediction <- predict(rf_mode1, sweetvaluepred[,-1])
-#sweetvaluepred.prediction <- data.frame(sweetvaluepred.prediction)
-data_value_all<-data.frame(SMILES=sweetvaluepred$SMILES,sweetvaluepred.prediction)
-#attr(unknowsweet.prediction,"probabilities")
-bb<-data.frame(SMILES = unknowsweet$SMILES,unknowsweet.prediction,id=c(1:24735))
-cc<-subset(bb,sweet>=0.5)
-sweetvalue<-data.frame()
-for (i in cc$SMILES){
-  dd<-subset(unknowsweet,SMILES==i)
-  sweetvalue<-rbind(sweetvalue,dd)
-  print(i)}
-ee<-left_join(cc,unknowsweet,by="SMILES")
-
-ee[,-c("type")]
-write.csv(data_value_all,file = "data_value_all1.csv")
+library(readxl)
+regression_demo <- read_excel("regression_demo.xlsx")
+load("RF_Model.RData")#调用模型
+desc_value<-read.table("desc-hg.txt",sep = ",")[,1]#调用描述符
+data_select<-regression_demo[,desc_value]
+data_value_pred<- data.frame(SMILES = regression_demo$SMILES,data_select)
+sweetvaluepred.prediction <- predict(rf_mode1, regression_demo[,-1])
+data_value_all<-data.frame(SMILES=regression_demo$SMILES,sweetvaluepred.prediction)
+sweetvalue <- data_value_all
+sweetvalue
--- 甜味数据预测.R
+++ 甜味数据预测.R
@@ -1,21 +1,11 @@
-load("C:/r_project/RF.RData")#调用模型
-#desc<-colnames(Dataframe_x)
-#write.table(desc,file="desc.txt",sep = ",")
-desc_1<-read.table("C:/r_project/甜味绘图/desc.txt",sep = ",")[,1]#调用描述符
-unknowsweet$id<-c(1:24735)
-aa<-unknowsweet[,desc_1]
-aa_scale <- scale(aa)
-data_pred<- data.frame(SMILES = unknowsweet$SMILES, aa_scale)
+library(readxl)
+classification_demo <- read_excel("classification_demo.xlsx")
+load("RF_classification.RData")#调用模型
+desc_1<-read.table("desc.txt",sep = ",")[,1]#调用描述符
+aa<-classification_demo[,desc_1]
+data_pred<- data.frame(SMILES = classification_demo$SMILES, aa)
 unknowsweet.prediction <- predict(Randommodel, data_pred[,-1],type="prob")
-#attr(unknowsweet.prediction,"probabilities")
-bb<-data.frame(SMILES = unknowsweet$SMILES,unknowsweet.prediction,id=c(1:24735))
-cc<-subset(bb,sweet>=0.5)
-sweetvalue<-data.frame()
-for (i in cc$SMILES){
-  dd<-subset(unknowsweet,SMILES==i)
-  sweetvalue<-rbind(sweetvalue,dd)
-  print(i)}
-ee<-left_join(cc,unknowsweet,by="SMILES")
-
-ee[,-c("type")]
-write.csv(ee,file = "sweetvaluepred.csv")
+unknowsweet.prediction <- data.frame(unknowsweet.prediction)
+bb<-data.frame(SMILES = classification_demo$SMILES,unknowsweet.prediction)
+result <- subset(bb, sweet >= 0.5)
+result
